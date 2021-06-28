ed54e018aa4ace607cbcd75fc0ee353a7b91b9c3
isakbjrkman <isak.bjorkman@aalto.fi>
2021-06-28 09:10:04 +0300

    first commit

---
+++ build/CMakeFiles/exampleB5.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/isak/Desktop/geant4/ProjectModel/exampleB5.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/exampleB5.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5ActionInitialization.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5ActionInitialization.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5DetectorConstruction.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5DetectorConstruction.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5DriftChamberHit.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5DriftChamberHit.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5DriftChamberSD.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5DriftChamberSD.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5EventAction.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5EventAction.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5PrimaryGeneratorAction.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5PrimaryGeneratorAction.cc.o"
+  "/home/isak/Desktop/geant4/ProjectModel/src/B5RunAction.cc" "/home/isak/Desktop/geant4/ProjectModel/build/CMakeFiles/exampleB5.dir/src/B5RunAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/isak/Documents/geant4/geant4.10.07.p02-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/exampleB5.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exampleB5.dir/exampleB5.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5ActionInitialization.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5DetectorConstruction.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5DriftChamberHit.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5DriftChamberSD.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5EventAction.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/exampleB5.dir/src/B5RunAction.cc.o"
+  "exampleB5"
+  "exampleB5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exampleB5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
