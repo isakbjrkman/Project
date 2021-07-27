a80a9370ab7e076ace054a7bfe623020034fcd39
isakbjorkman <isak2448@gmail.com>
2021-07-27 15:37:12 +0300

    2D histograms added to rootMacro

--- build/rootMacro.cpp
+++ build/rootMacro.cpp
@@ -62,16 +62,21 @@ double h = 6.62607015*pow(10.,-34);
 double c = 299792458;
 double E;
 
-TH1D* h1ev = new TH1D("h1ev", "EventID; Event ID; frequency", 10000, 0, nentries/2000);
-TH1D* h1de = new TH1D("h1de", "DetectorID; Detector ID; frequency", 50, 0.5, 4.5);
-TH1D* h1pdg = new TH1D("h1pdg", "PDG; pdg; frequency", 100, -50, 50);
-TH1D* h1px = new TH1D("h1px", "PX; px (MeV); frequency", 100, 8*pow(10., -6), 8*pow(10., -6));
-TH1D* h1py = new TH1D("h1py", "PY; py (MeV); frequency", 100, 8*pow(10., -6), 8*pow(10., -6));
-TH1D* h1pz = new TH1D("h1pz", "PZ; pz (MeV); frequency", 100, 0, 8*pow(10., -6));
-TH1D* h1x = new TH1D("h1x", "X; x (mm); frequency", 200, -30, 30);
-TH1D* h1y = new TH1D("h1y", "Y; y (mm); frequency", 200, -30, 30);
-TH1D* h1z = new TH1D("h1z", "Z; z (mm); frequency", 200, 11.99, 12.015);
-TH1D* h1w = new TH1D("h1w", "Wavelength distribution; wavelength (nm); frequency", 100, 0.06*pow(10.,3), 0.53*pow(10., 3));
+TH1D* h1ev = new TH1D("h1ev", "EventID; Event ID; Frequency", 10000, 0, nentries/320);
+TH1D* h1de = new TH1D("h1de", "DetectorID; Detector ID; Frequency", 50, 0.5, 4.5);
+TH1D* h1pdg = new TH1D("h1pdg", "PDG; pdg; Frequency", 100, -50, 50);
+TH1D* h1px = new TH1D("h1px", "PX; px (MeV); Frequency", 200, 8*pow(10., -6), 8*pow(10., -6));
+TH1D* h1py = new TH1D("h1py", "PY; py (MeV); Frequency", 200, 8*pow(10., -6), 8*pow(10., -6));
+TH1D* h1pz = new TH1D("h1pz", "PZ; pz (MeV); Frequency", 200, 0, 8*pow(10., -6));
+TH1D* h1x = new TH1D("h1x", "X; x (mm); Frequency", 200, -30, 30);
+TH1D* h1y = new TH1D("h1y", "Y; y (mm); Frequency", 200, 47, 102);
+TH1D* h1z = new TH1D("h1z", "Z; z (mm); Frequency", 200, 3354.995, 3355.015);
+TH1D* h1w = new TH1D("h1w", "Wavelength distribution; Wavelength (nm); frequency", 200, 0.06*pow(10.,3), 0.53*pow(10., 3));
+
+TH2F* h2xy = new TH2F("h2xy", "X-Y hits",  100, -31, 31,1000, 44, 105);
+TH2F* h2xz = new TH2F("h2xz", "X-Z hits",  100, -30, 30,1000, 3354.993, 3355.010);
+h2xy->SetStats(0);
+h2xz->SetStats(0);
 
 double convert = 1.60217662*pow(10.,-13);   //MeV to Joules
 
@@ -89,8 +94,14 @@ for (int i=0; i<nentries; i++) {
   h1x->Fill(x);
   h1y->Fill(y);
   h1z->Fill(z);
+  h2xy->Fill(x,y);
+  h2xz->Fill(x,z);  
 }  
 
+h2xy->SetContour(1000);
+h2xz->SetContour(1000);
+h2xy->SetOption("colz");
+h2xz->SetOption("colz");
 
 h1w->Write();
 h1ev->Write();
@@ -102,6 +113,9 @@ h1pz->Write();
 h1x->Write();
 h1y->Write();
 h1z->Write();
+
+h2xy->Write();
+h2xz->Write();
 
 f->Close();
 f1->Close();
